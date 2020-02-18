d905bd9604359944528cff75583dc4f9577a07b6
Eric C. Weig <eweig@email.uky.edu>
2020-02-18 15:00:46 -0500

    Update GDriveManifest.js

--- GDriveManifest.js
+++ GDriveManifest.js
@@ -1,6 +1,7 @@
 // Adapted from Code written https://www.acrosswalls.org/ortext-datalinks/list-google-drive-folder-file-names-urls/ - accessed 2020-02-18
 // Above code designated CCO 1.0 Universal Public Domain https://www.acrosswalls.org/about/ - accessed 2020-02-18
 // Replace the text target-folder below with the folder for which you want a listing
+// -----  copy the code below this line to insert in Google Sheet code editor  ----- 
 
 function gdrivemanifest() {
   var gdrive_folder_name = 'target-folder';
